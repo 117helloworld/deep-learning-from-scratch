7ae5009f8f9eaa6264c8a6eff9aa1cfbb192dcd5
Takayasu Sugiyama <117helloworld@gmail.com>
2022-09-10 05:11:54 +0900

    first time commit

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -1,11 +1,16 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import pickle
-from dataset.mnist import load_mnist
-from common.functions import sigmoid, softmax
+from mnist import load_mnist
 
+def sigmoid(x):
+    return 1/(1+np.exp(x))
+
+def softmax(x):
+    c=np.max(x)
+    exp=np.exp(x-c)
+    sum_exp=np.sum(exp)
+    return exp/sum_exp
 
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
@@ -13,32 +18,37 @@ def get_data():
 
 
 def init_network():
-    with open("sample_weight.pkl", 'rb') as f:
+    with open("C:\\Users\\math2\\OneDrive\\ドキュメント\\VSCode\\Python\\deep_learning_from_scratch\\ch03\\sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
 
 
-def predict(network, x):
+def predict(network, x): # x is matrix
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
-
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = softmax(a3)
-
+    
+    a1=np.dot(x,W1)+b1
+    z1=sigmoid(a1)
+    a2=np.dot(z1,W2)+b2
+    z2=sigmoid(a2)
+    a3=np.dot(z2,W3)+b3
+    y=softmax(a3)
     return y
 
 
-x, t = get_data()
+x, t = get_data() # x is 行列. t is the answer.
 network = init_network()
 accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p= np.argmax(y) # 最も確率の高い要素のインデックスを取得
-    if p == t[i]:
-        accuracy_cnt += 1
+y = predict(network, x)
+print(np.shape(y))
+
+
+batch_size=100
+for i in range(0,len(x),batch_size):
+    y=predict(network,x[i:i+batch_size])
+    p=np.argmax(y,axis=1)
+    T=t[i:i+batch_size]
+    accu= p==T
+    accuracy_cnt+=sum(accu)
 
 print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
